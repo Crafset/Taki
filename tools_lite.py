724c39e374627a93d3df51a2edfa3d385ffab64a
Crafset <82468619+Crafset@users.noreply.github.com>
2021-08-22 18:53:39 +0200

    Update tools_lite.py

--- tools_lite.py
+++ tools_lite.py
@@ -1,5 +1,18 @@
 import os
 
+print
+print("___________________    _____  __________________________________________")
+print("\_   ___ \______   \  /  _  \ \_   _____/   _____/\_   _____/\__    ___/")
+print("/    \  \/|       _/ /  /_\  \ |    __) \_____  \  |    __)_   |    ")
+print("\     \___|    |   \/    |    \|     \  /        \ |        \  |    |")
+print(" \______  /____|_  /\____|__  /\___  / /_______  //_______  /  |____|")
+print("        \/       \/         \/     \/          \/         \/")
+print ("Crafset  : AZERT-HACK")
+print ("Team : CYBER ATTACK ")
+print ("Discord : https://discord.gg/GcQAWW4kUZ")
+print
+print("______________________________________")
+
 name = input("Enter name : ")
 hostname = input("Enter hostname")
 
